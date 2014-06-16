6bc73471ff6b78154be35958c986b45764fdb232
Diogo <d.alves@personare.com.br>
2014-06-16 18:34:51 -0300

    Layout da página principal

--- InterativaCPII/protected/views/layouts/main.php
+++ InterativaCPII/protected/views/layouts/main.php
@@ -51,13 +51,13 @@
 
 			</div>
 		</div>
+		<div class="right_column">
+			<div class="social">
+			    <a target="_blank" href="https://twitter.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/twitter_icon.png" alt="Twitter Radio Interativa"/></a>
+			    <a target="_blank" href="https://www.facebook.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/facebook_icon.png" alt="Facebook Radio Interativa"/></a>
+			    <a target="_blank" href="https://www.youtube.com/channel/UC0qOMx4D_4nFe-6m-syQ7Hg"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/youtube_icon.png" alt="Youtube Radio Interativa"/></a>
+			</div>
 	</div>
-	<div class="right_column">
-<div class="social">
-    <a target="_blank" href="https://twitter.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/twitter_icon.png" alt="Twitter Radio Interativa"/></a>
-    <a target="_blank" href="https://www.facebook.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/facebook_icon.png" alt="Facebook Radio Interativa"/></a>
-    <a target="_blank" href="https://www.youtube.com/channel/UC0qOMx4D_4nFe-6m-syQ7Hg"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/youtube_icon.png" alt="Youtube Radio Interativa"/></a>
-</div>
 
 	<div class="clear"></div>
 
