8da58bec5abbebd8e0f98a3e0fd512622010239c
Diogo <d.alves@personare.com.br>
2014-06-16 18:32:15 -0300

    Layout da página principal

--- InterativaCPII/protected/views/layouts/main.php
+++ InterativaCPII/protected/views/layouts/main.php
@@ -34,10 +34,6 @@
 			'itemCssClass'=>'menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-44',
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index'),
-
-					'linkOptions'=>array(
-						'id'=>'oi',
-					),
 				),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
@@ -47,13 +43,21 @@
 		)); ?>
 	</div>
 	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
+	
+	<div class="middle_bar">
+		<div class="left_column">
+			<div class="noticias">
 	<?php echo $content; ?>
+
+			</div>
+		</div>
+	</div>
+	<div class="right_column">
+<div class="social">
+    <a target="_blank" href="https://twitter.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/twitter_icon.png" alt="Twitter Radio Interativa"/></a>
+    <a target="_blank" href="https://www.facebook.com/cp2interativa"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/facebook_icon.png" alt="Facebook Radio Interativa"/></a>
+    <a target="_blank" href="https://www.youtube.com/channel/UC0qOMx4D_4nFe-6m-syQ7Hg"><img src="http://www.radiocp2interativa.com.br/wp-content/themes/blankslate/img/youtube_icon.png" alt="Youtube Radio Interativa"/></a>
+</div>
 
 	<div class="clear"></div>
 
