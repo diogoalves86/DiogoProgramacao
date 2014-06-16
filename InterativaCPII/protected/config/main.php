ea765f307e1a6a2c73fd93fa78088b22fb58933b
Diogo <d.alves@personare.com.br>
2014-06-16 18:44:14 -0300

    Sistema da rádio

--- InterativaCPII/protected/config/main.php
+++ InterativaCPII/protected/config/main.php
@@ -47,15 +47,15 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=InterativaCPII',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => '123',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
