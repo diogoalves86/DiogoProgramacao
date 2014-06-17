89faeb8f9a0c3ea7774d0abeec3b4de23eea5cbe
Diogo <diogoalvessilva86@gmail.com>
2014-06-16 23:22:11 -0300

    Controller do User

---
+++ InterativaCPII/protected/views/post/_form.php
@@ -0,0 +1,58 @@
+<?php
+/* @var $this PostController */
+/* @var $model Post */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'post-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'idUser'); ?>
+		<?php echo $form->textField($model,'idUser'); ?>
+		<?php echo $form->error($model,'idUser'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>400)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'message'); ?>
+		<?php echo $form->textArea($model,'message',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'message'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'creationTime'); ?>
+		<?php echo $form->textField($model,'creationTime'); ?>
+		<?php echo $form->error($model,'creationTime'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'updateTime'); ?>
+		<?php echo $form->textField($model,'updateTime'); ?>
+		<?php echo $form->error($model,'updateTime'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ InterativaCPII/protected/views/post/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this PostController */
+/* @var $data Post */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('idUser')); ?>:</b>
+	<?php echo CHtml::encode($data->idUser); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('message')); ?>:</b>
+	<?php echo CHtml::encode($data->message); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('creationTime')); ?>:</b>
+	<?php echo CHtml::encode($data->creationTime); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updateTime')); ?>:</b>
+	<?php echo CHtml::encode($data->updateTime); ?>
+	<br />
+
+
+</div>
