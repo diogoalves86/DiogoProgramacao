135af40a02a7aca0091e6a104020c97df6ff9576
Diogo <diogoalvessilva86@gmail.com>
2014-06-18 21:57:00 -0300

    Sistema de autenticação

--- InterativaCPII/protected/components/UserIdentity.php
+++ InterativaCPII/protected/components/UserIdentity.php
@@ -15,19 +15,21 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
+
+
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
+
+		$record = User::model()->findByAttributes(array('login'=>$this->username));
+
+		if($record === null){
+			$this->errorCode = self::ERROR_USERNAME_INVALID;
+		} elseif ($record->password !== $this->password){
+			$this->errorCode = self::ERROR_PASSWORD_INVALID;
+		} else {
+			$this->username=$record->login;
 			$this->errorCode=self::ERROR_NONE;
+		}
 		return !$this->errorCode;
 	}
 }
