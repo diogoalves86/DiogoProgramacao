ec2686ad02e2944f8282175e418b429fda938574
Diogo <diogoalvessilva86@gmail.com>
2014-06-19 02:46:45 -0300

    Implementando app

--- PHPPageReader/index.php
+++ PHPPageReader/index.php
@@ -1,4 +1,6 @@
 <?php
-require_once('protected/ReadPage.class.php');
-$obj = new ReadPage("http://radiointerativacp2.com.br");
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require('protected/ReadPage.class.php');
+$obj = new ReadPage($url = "http://radiointerativacp2.com.br/");
 ?>
--- PHPPageReader/protected/ReadPage.class.php
+++ PHPPageReader/protected/ReadPage.class.php
@@ -1,27 +1,30 @@
 <?php 
 require('protected/Run.class.php');
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 class ReadPage
 {
-	private $url;
+	public $url = "http://google.com/";
 
 	public function __construct($url){
 		$run = new Run();
+		$this->url = $url;
 		if($run->CanRun() === true)
 			$this->Start($url);	
 	}
 
+	public function GetUrl(){
+		return $this->url;
+	}
 
 	private function Start($url){
 		try{
 			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_VERBOSE, true);
+		  	curl_setopt($curl, CURLOPT_HEADER, true); 
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($curl, CURLOPT_URL, $this->url);
 			$result = curl_exec($curl);
-			return $result;
+			var_dump($result);
 		}
 		catch(Exception $ex){
 			return $ex->getMessage();
