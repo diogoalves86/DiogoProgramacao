7a820474f7be554a9bd77cfa653a12fe77b4a0cf
Diogo Alves <diogoalvessilva86@gmail.com>
2015-01-20 19:08:15 -0200

    Implementando a verificação específica para os var_dumps()

--- Python/automatizer.py
+++ Python/automatizer.py
@@ -15,17 +15,35 @@ class Automatizer:
         processo = subprocess.check_output([self.comando], shell=True)
         return processo
 
+    def processa_texto_por_string(self, stringPesquisada):
+        arquivos = []
+        textoParcial = self.processo
+        #Lógica para a identificação de todos os arquivos modificados
+        for i in range(0, self.processo.count(stringPesquisada)):
+            indice = textoParcial.find(stringPesquisada)
+            textoParcial = textoParcial[indice:]
+            nomeArquivo = textoParcial[len(stringPesquisada):]
+            nomeArquivo = nomeArquivo[1:nomeArquivo.index("\n")]
+            textoParcial = textoParcial[textoParcial.index(nomeArquivo):]
+            resultado = textoParcial
+            arquivos.append(nomeArquivo)
+        return arquivos
+
     def verifica_antes_commitar(self):
-        if "var_dump(" in self.processo:
-            print("Foi detectado um var_dump() no seu código!")
-            print("Estas alterações ainda não foram salvas, atente-se para alterá-las antes de commitar!")
-            print("COMANDO DIGITADO: \"%s\". OUTPUT SEGUE ABAIXO: \n" % self.comando)
-            print(self.processo + "\n")
-            print("Aplicação encerrada.")
-            exit()
-        else:
-            resposta = input("Não foi encontrado var_dump() no seu código, deseja commitá-lo ? (s) ou (n)")
-            return resposta
+        self.arquivos = self.processa_texto_por_string("modified:  ")
+        for arquivo in self.arquivos:
+            self.processo = self.executa_comando("git show %s" % arquivo)
+            if "var_dump(" in self.processo:
+                print("Foi detectado um var_dump() no seu código!")
+                print("Estas alterações ainda não foram salvas, atente-se para alterá-las antes de commitar!")
+                print("COMANDO DIGITADO: \"%s\". OUTPUT SEGUE ABAIXO: \n" % self.comando)
+                print(self.processo + "\n")
+                print("Aplicação encerrada.")
+                exit()
+            else:
+                resposta = input("Não foi encontrado var_dump() no seu código, deseja commitá-lo ? (s) ou (n)")
+                return resposta
+
 
     def verifica_commit(self):
         if "var_dump(" in self.processo:
@@ -36,10 +54,10 @@ class Automatizer:
             print("O seu código não apresenta var_dump()")
 
     def var_dump_finder(self):
-        self.processo = self.executa_comando("git show")
+        self.processo = self.executa_comando("git status")
+        self.verifica_antes_commitar()
         self.hash = self.processo[7:48]
         self.processo = self.executa_comando("git diff --cached")
-        self.verifica_antes_commitar()
         self.processo = self.executa_comando("git show %s" % self.hash)
         self.verifica_commit()
 
