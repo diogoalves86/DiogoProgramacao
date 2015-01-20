b53a247b6dea17c16b50fda294567d6bf5997e67
Diogo Alves <diogoalvessilva86@gmail.com>
2015-01-20 00:02:34 -0200

    Commits de teste e ATUALIZANDO O AUTOMATIZER

--- Python/automatizer.py
+++ Python/automatizer.py
@@ -11,17 +11,21 @@ class Automatizer:
         self.comando = novoValor
 
     def executa_comando(self, comando):
-        processo = subprocess.check_output([comando], shell=True)
+        self.comando = comando
+        processo = subprocess.check_output([self.comando], shell=True)
         return processo
 
     def verifica_antes_commitar(self):
         if "var_dump(" in self.processo:
             print("Foi detectado um var_dump() no seu código!")
             print("Estas alterações ainda não foram salvas, atente-se para alterá-las antes de commitar!")
-            print("COMANDO DIGITADO: \"git diff .\". OUTPUT SEGUE ABAIXO: \n")
+            print("COMANDO DIGITADO: \"%s\". OUTPUT SEGUE ABAIXO: \n" % self.comando)
             print(self.processo + "\n")
             print("Aplicação encerrada.")
             exit()
+        else:
+            resposta = input("Não foi encontrado var_dump() no seu código, deseja commitá-lo ? (s) ou (n)")
+            return resposta
 
     def verifica_commit(self):
         if "var_dump(" in self.processo:
@@ -34,7 +38,7 @@ class Automatizer:
     def var_dump_finder(self):
         self.processo = self.executa_comando("git show")
         self.hash = self.processo[7:48]
-        self.processo = self.executa_comando("git diff .")
+        self.processo = self.executa_comando("git diff --cached")
         self.verifica_antes_commitar()
         self.processo = self.executa_comando("git show %s" % self.hash)
         self.verifica_commit()
