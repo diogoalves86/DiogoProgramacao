3b42e78eb23d144c70c7673929b707ff3331fb04
Diogo Alves <diogoalvessilva86@gmail.com>
2015-01-20 19:41:25 -0200

    Commit de testes e implementando o var_dump() Finder.

--- Python/automatizer.py
+++ Python/automatizer.py
@@ -25,40 +25,47 @@ class Automatizer:
             nomeArquivo = textoParcial[len(stringPesquisada):]
             nomeArquivo = nomeArquivo[1:nomeArquivo.index("\n")]
             textoParcial = textoParcial[textoParcial.index(nomeArquivo):]
-            resultado = textoParcial
             arquivos.append(nomeArquivo)
         return arquivos
 
     def verifica_antes_commitar(self):
         self.arquivos = self.processa_texto_por_string("modified:  ")
         for arquivo in self.arquivos:
-            self.processo = self.executa_comando("git show %s" % arquivo)
-            if "var_dump(" in self.processo:
+            print("Processando arquivo: %s" % arquivo)
+            processo = self.executa_comando("git diff %s" % arquivo)
+
+            if processo == "":
+                processo = self.executa_comando("git diff --cached %s" % arquivo)
+
+            if "var_dump(" in processo:
                 print("Foi detectado um var_dump() no seu código!")
                 print("Estas alterações ainda não foram salvas, atente-se para alterá-las antes de commitar!")
                 print("COMANDO DIGITADO: \"%s\". OUTPUT SEGUE ABAIXO: \n" % self.comando)
-                print(self.processo + "\n")
+                print(processo + "\n")
+                print("Arquivo %s processado" % arquivo)
                 print("Aplicação encerrada.")
                 exit()
             else:
-                resposta = input("Não foi encontrado var_dump() no seu código, deseja commitá-lo ? (s) ou (n)")
-                return resposta
+                #resposta = input("Não foi encontrado var_dump() no seu código, deseja commitá-lo ? (s) ou (n)")
+                return False
 
 
     def verifica_commit(self):
         if "var_dump(" in self.processo:
             print("Foi detectado uma função var_dump() no seu ultimo commit.")
-            print("Atente-se para corrigi-lo! Segue abaixo o commit para verificar e corrigir. \n")
-            print(self.processo)
+            print("""Atente-se para corrigi-lo! Segue abaixo o commit para verificar e corrigir.
+            Commit Hash: %s
+                  """ % self.commitHash)
         else:
             print("O seu código não apresenta var_dump()")
 
     def var_dump_finder(self):
         self.processo = self.executa_comando("git status")
         self.verifica_antes_commitar()
-        self.hash = self.processo[7:48]
+        self.processo = self.executa_comando("git show")
+        self.commitHash = self.processo[7:48]
         self.processo = self.executa_comando("git diff --cached")
-        self.processo = self.executa_comando("git show %s" % self.hash)
+        self.processo = self.executa_comando("git show %s" % self.commitHash)
         self.verifica_commit()
 
 
