c1070481ec631c3e9d74873b5db01c6fdd3b0c68
Diogo Alves <diogoalvessilva86@gmail.com>
2015-02-16 00:44:34 -0200

    Movendo "Projeto Interativa CP2" para o repositório criado.

--- RadioInterativa/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=InterativaCPII',
-    'username' => 'root',
-    'password' => '123',
-    'charset' => 'utf8',
-];
--- RadioInterativa/views/site/index.php
+++
@@ -1,8 +0,0 @@
-<div class="noticia">
-    <div class="titulo">
-        <h2>Teste</h2>
-    </div>
-    <div class="conteudo">
-        <p>Conteúdo</p>
-    </div>
-</div>       
